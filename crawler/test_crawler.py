bae62ec95d397437c2db0710d1c045a0bcffe606
Taeoh Kim <kimtaeoh95@gmail.com>
2019-04-01 09:11:58 +0900

    Finish External Crawler

--- crawler/test_crawler.py
+++ crawler/test_crawler.py
@@ -1,3 +1,22 @@
 import pytest
+from base_cralwer import BaseCrawler, basic_crawl
+import datetime
+import requests
+from bs4 import BeautifulSoup
+from logger import getMyLogger
 
 # TODO
+class EmptyCrawler(BaseCrawler):
+
+        
+    def crawl(self):
+        pass
+
+    def parse(self):
+        pass
+
+    def save(self):
+        print(self)
+
+    def __str__(self):
+        return "This is Empty Crawler only for test"
--- crawler/yonsei_crawler.py
+++ crawler/yonsei_crawler.py
@@ -6,7 +6,7 @@ from logger import getMyLogger
 
 mylogger = getMyLogger()
 
-class NoticeCrawler(BaseCrawler):
+class YonseiCrawler(BaseCrawler):
 
     def __init__(self):
         # https://www.yonsei.ac.kr/sc/support/notice.jsp
@@ -14,8 +14,6 @@ class NoticeCrawler(BaseCrawler):
         super().__init__()
         self.html = None
         self._datetime = datetime.datetime.now()
-        self.url = "https://www.yonsei.ac.kr/sc/support/notice.jsp"
-        self.sub_crawler = NoticeMainCrawler
         self.crawl_method = basic_crawl
     
         
@@ -44,7 +42,7 @@ class NoticeCrawler(BaseCrawler):
             mylogger.info(self.data)
 
     def __str__(self):
-        return f"<Notice Crawler> | Datetime : {self._datetime} | Address : {self.url}"
+        return f"<Yonsei Crawler> | Datetime : {self._datetime} | Address : {self.url}"
 
     def _parse_list(self, board):
         try:
@@ -60,6 +58,28 @@ class NoticeCrawler(BaseCrawler):
         except Exception as e:
             mylogger.warn(e)
             return None
+
+class NoticeCrawler(YonseiCrawler):
+
+    def __init__(self):
+        super().__init__()
+        self.url = "https://www.yonsei.ac.kr/sc/support/notice.jsp"
+        self.sub_crawler = NoticeMainCrawler
+
+    def __str__(self):
+        return f"<Notice Crawler> | Datetime : {self._datetime} | Address : {self.url}"
+
+class ExternalCrawler(YonseiCrawler):
+
+    def __init__(self):
+        super().__init__()
+        self.url = "https://www.yonsei.ac.kr/sc/support/etc_notice.jsp"
+        self.sub_crawler = NoticeMainCrawler
+        # from test_crawler import EmptyCrawler
+        # self.sub_crawler = EmptyCrawler
+
+    def __str__(self):
+        return f"<External Crawler> | Datetime : {self._datetime} | Address : {self.url}"
 
 class NoticeMainCrawler(BaseCrawler):
 
@@ -81,7 +101,7 @@ class NoticeMainCrawler(BaseCrawler):
             
         board_view = soup.find('dl', {'class' : 'board_view'})
         title = soup.find('title').text
-        date = board_view.find('span', {'class' : 'date'})
+        date = board_view.find('span', {'class' : 'date'}).text
         cont_area = board_view.find('div', {'class' : 'cont_area'})
         data = {'title' : title, 'date' : date, 'cont_area' : cont_area}
 
@@ -96,5 +116,5 @@ class NoticeMainCrawler(BaseCrawler):
         return f"<Notice Main Crawler> |  Address : {self.url}"
 
 if __name__=='__main__':
-    n = NoticeCrawler()
+    n = ExternalCrawler()
     n.start()
